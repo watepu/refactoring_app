ef86afce75c87493091bb74e9c65e06bb8a5f7ef
watepu <40508321+watepu@users.noreply.github.com>
2018-11-05 12:18:55 +0900

    コールバックの切り出し

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,16 +1,6 @@
 class Blog < ApplicationRecord
   include CommonModule
   # validates_with BlogValidator
-  before_create :default_title
-  before_create :default_content
-
-  private
-  def default_title
-    self.title = 'タイトルなし' if self.title.blank?
-  end
-
-  def default_content
-    self.content = '内容なし' if self.content.blank?
-  end
+  before_create BlogCallback.new
 
 end
