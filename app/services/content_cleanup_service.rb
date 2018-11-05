d757716f4c8bd858d0a7566fe60f3b826f24694c
watepu <40508321+watepu@users.noreply.github.com>
2018-11-05 12:33:31 +0900

    全角を半角へ、文頭文末の空白を削除する処理を記述

---
+++ app/services/content_cleanup_service.rb
@@ -0,0 +1,24 @@
+class ContentCleanupService
+  def initialize(blog)
+    @blog = blog
+    @content = blog.content
+  end
+
+  def call
+    full_to_half
+    blank_delete
+  end
+
+  private
+    attr_reader :blog, :content
+
+    def full_to_half
+      @content = @content.tr('0-9a-zA-Z').gsub!(//,'')
+      @blog.update(content: @content)
+    end
+
+    def blank_delete
+      @content = @content.strip
+      @blog.update(content: @content)
+    end
+end
